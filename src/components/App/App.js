fee71cea588ccfad0bdfd12c86864722e8d16310
vat777 <vitalii.taranchuk@gmail.com>
2023-10-03 18:54:48 +0300

    Add body, header and static search form in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,22 @@
 import logo from '../../assets/logo.svg'
 import './App.css';
+import '../../assets/general.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Ja<span className='purple'>mmm</span>ing</h1>
       </header>
-    </div>
+
+      <form className='search-container'>
+        <input className='search-input' type="text" />
+        <button className='button'>SEARCH</button>
+      </form>
+
+      
+
+    </>
   );
 }
 
